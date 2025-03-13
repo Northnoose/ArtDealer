fec973b880da66062c389b757ff59d097fe125ad
Russamadda <deivi.selenis@gmail.com>
2025-03-13 10:39:36 +0100

    Merget med master

--- app/src/main/java/com/example/artdealer/data/SelectedPhoto.kt
+++ app/src/main/java/com/example/artdealer/data/SelectedPhoto.kt
@@ -1,4 +1,9 @@
 package com.example.artdealer.data
 
 data class SelectedPhoto(
+    val photo: Photo,
+    val frameType: FrameType,
+    val frameWidth: Int,
+    val photoSize: PhotoSize,
+    val finalPrice: Float
 )
